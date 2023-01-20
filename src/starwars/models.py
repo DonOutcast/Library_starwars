4faf642cae8fa71479c911d9dde4b4ef226298bb
DonOutcast <lymondgl@student.21-school.ru>
2023-01-20 23:59:04 +0300

    need add multiple class

--- src/starwars/models.py
+++ src/starwars/models.py
@@ -4,8 +4,9 @@ try:
     import sys
     import time
     import json
-    from utils import query, all_resource_urls
     from settings import Config
+    from exceptions import ResourceDoesNotExists
+    from utils import query, all_resource_urls
 except (Exception,):
     print(sys.exc_info())
 
@@ -87,7 +88,7 @@ class Starship(BaseModel):
 
     # def get_films(self):
     # return FilmQuerySet(self.films)
-
+    #
     # def get_pilots(self):
     # return PeopleQuerySet(self.pilots)
 
@@ -137,5 +138,151 @@ conf = Config()
 #
 #
 # print(get_all_names_and_id("{0}/{1}/".format(conf.get_url_api(), "people")))
+def _get(id, type, conf):
+    ''' Return a single person '''
+    result = query("{0}/{1}/{2}/".format(
+        conf.get_url_api(),
+        type,
+        str(id))
+    )
+    return result
 
 
+def get_starship(starship_id, conf):
+    """ Return a single starship """
+    result = _get(starship_id, conf.get_starships(), conf)
+    return Starship(result.content)
+
+
+class BaseRequest:
+
+    def __init__(self, id_search, url_path):
+        super().__init__()  
+        response_json = query("{0}/{1}".format(url_path, id_search))
+        # self.json_data = response_json
+        self.id = id_search
+        # self.json_data = json.loads(raw_data)
+
+
+class StarshipT(Config, BaseRequest):
+    """
+    Class for all names and ids in world Star Wars.
+        Usage:
+
+    .. code-block:: python3
+        :caption: Creating instance of StarWars
+
+        from star_wars import star_wars
+        jedi = StarWars(
+        # and use jedi methods.
+
+        :param id_starship: of a starship, should be obtained from StarWars
+        :type id_starship: :obj: `int`
+    """
+
+    def __init__(self, id_starship: int):
+        # super(StarshipT, self).__init__(id_starship, Config.get_url_api() + Config.get_starships())
+        BaseRequest.__init__(self, id_starship, Config.get_url_api() + Config.get_starships())
+        # self.id = id_starship
+        self.json_data = BaseRequest.json_data
+    #
+    # def get_json_data(self):
+    #     return self.json_data
+
+print(StarshipT.__mro__)
+temp = StarshipT(2)
+print(temp)
+# READY
+# import json
+# import requests
+# from settings import Config
+#
+#
+#
+# class StarWars:
+#     """
+#     Class for all names and ids in world Star Wars.
+#         Usage:
+#
+#     .. code-block:: python3
+#         :caption: Creating instance of StarWars
+#
+#         from star_wars import star_wars
+#         jedi = StarWars(
+#         # and use jedi methods.
+#     """
+#
+#     def __init__(self):
+#         self.__url_people = Config.get_url_api() + Config.get_people()
+#         self.__url_planets = Config.get_url_api() + Config.get_planets()
+#         self.__url_films = Config.get_url_api() + Config.get_films()
+#         self.__url_species = Config.get_url_api() + Config.get_species()
+#         self.__url_vehicles = Config.get_url_api() + Config.get_vehicles()
+#         self.__url_starships = Config.get_url_api() + Config.get_starships()
+#
+#     @staticmethod
+#     def get_response(url_path: str, name_key: str) -> dict[str, int]:
+#         """ Return a response to url
+#          :param: url_path str
+#          :type: str
+#          :param: name_key this is a key of value
+#          :type: str
+#          :returns: Response to url in a dictionary
+#          :type: dict[str, int]
+#         """
+#         running = True
+#         names = {}
+#         while running:
+#             my_response = requests.get(url_path)
+#             if my_response.status_code != 200:
+#                 raise ResourceDoesNotExists
+#             json_data = json.loads(my_response.content)
+#             for resource in json_data["results"]:
+#                 names[resource.get(name_key)] = resource.get("url").split("/")[-2]
+#             if bool(json_data.get("next")):
+#                 url_path = json_data["next"]
+#             else:
+#                 running = False
+#         return names
+#
+#     def get_people_names(self):
+#         """ Return all names and ids of people
+#          :returns: Response to url in a dictionary
+#          :type: dict[str, int]
+#         """
+#         return self.get_response(self.__url_people, name_key="name")
+#
+#     def get_planets_names(self):
+#         """ Return all names and ids of planets
+#          :returns: Response to url in a dictionary
+#          :type: dict[str, int]
+#         """
+#         return self.get_response(self.__url_planets, name_key="name")
+#
+#     def get_films_names(self):
+#         """ Return all names and ids of films
+#          :returns: Response to url in a dictionary
+#          :type: dict[str, int]
+#         """
+#         return self.get_response(self.__url_films, name_key="title")
+#
+#     def get_species_names(self):
+#         """ Return all names and ids of species
+#          :returns: Response to url in a dictionary
+#          :type: dict[str, int]
+#         """
+#         return self.get_response(self.__url_species, name_key="name")
+#
+#     def get_vehicles_names(self):
+#         """ Return all names and ids of vehicles
+#          :returns: Response to url in a dictionary
+#          :type: dict[str, int]
+#         """
+#         return self.get_response(self.__url_vehicles, name_key="name")
+#
+#     def get_starships_names(self):
+#         """ Return all names and ids of starships
+#          :returns: Response to url in a dictionary
+#          :type: dict[str, int]
+#         """
+#         return self.get_response(self.__url_starships, name_key="name")
