d2e75c106be45f26566fd2399b9302ecba5f6f82
DonOutcast <lymondgl@student.21-school.ru>
2023-01-27 23:42:01 +0300

    class people begin

--- src/star_wars/utils/starships.py
+++ src/star_wars/utils/starships.py
@@ -6,86 +6,10 @@ try:
     from lxml import etree
     from typing import List, Any
     from bs4 import BeautifulSoup
-    from src.star_wars.utils_1 import query
     from src.star_wars.settings import Config
-    from src.star_wars.exceptions import ResourceDoesNotExists
+    from src.star_wars.utils.baserequest import BaseRequest
 except (Exception,) as e:
     print(sys.exc_info())
-
-
-class BaseRequest:
-    """
-    The Class generate response with json
-    :param id_search: id of people or starships or planets or films or species or vehicles
-    :type id_search: :obj: `int`
-    :param url_path: the path from Config
-    :type url_path: :obj: `str`
-    :return: If status code request is 200 a json data or Exception
-    :type: :obj: `json`
-    """
-
-    def __init__(self, id_search: int, url_path: str) -> None:
-        response_json = query("{0}/{1}".format(url_path, id_search))
-        self.json_data = response_json.json()
-        self.all_jsons = []
-        self.all_pages(url_path)
-
-    # @staticmethod
-    @staticmethod
-    def _check_status_code(url_path):
-        """
-        The function a check status code of get or post requests
-        :param url_path: Url path
-        :type: :obj:  `str`
-        :return: True if status code == 200 else raise
-        :type: :obj: `bool`
-        """
-        status = requests.get(url_path)
-        if status.status_code != 200:
-            raise ResourceDoesNotExists
-        return True
-
-    def _get_items_of_json(self, key_class: str, key_tag, url_path: str) -> list[Any]:
-        """ Return all pilots of the starship
-            :param key_class: Word for search example  starships, films
-            :type key_class: :obj: `str`
-            :param key_tag: Word for search in json example name, title
-            :type key_tag: :obj: `str`
-            :param url_path: Url for page in API
-            :type url_path: :obj: `str`
-            :return: List of names
-            :type: list[str]
-        """
-        running = True
-        names = []
-        while running:
-            my_response = requests.get(url_path)
-            if my_response.status_code != 200:
-                raise ResourceDoesNotExists
-            json_data = json.loads(my_response.content)
-            for resource in json_data["results"]:
-                for starship in resource.get(key_class):
-                    if starship.split("/")[-2] == str(self.id):
-                        names.append(resource.get(key_tag))
-            if bool(json_data.get("next")):
-                url_path = json_data["next"]
-            else:
-                running = False
-        return names
-
-    def all_pages(self, url_path) -> None:
-        """
-
-        :param url_path:
-        :return:
-        """
-        self._check_status_code(url_path)
-        response = requests.get(url_path)
-        json_data = response.json()
-        if json_data.get("next"):
-            url_path = json_data.get("next")
-            self.all_jsons.append(json_data)
-            return BaseRequest.all_pages(self, url_path)
 
 
 class Starship(BaseRequest):
