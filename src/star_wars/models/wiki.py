c39697b1fe6e4decd272948cbfa9196268cb4a36
DonOutcast <lymondgl@student.21-school.ru>
2023-02-03 11:26:25 +0300

    search photo droid

--- src/star_wars/models/wiki.py
+++ src/star_wars/models/wiki.py
@@ -15,16 +15,28 @@ except (Exception,) as e:
 
 class Wiki:
 
-    def __init__(self, name_for_searching: str, url_path: str):
-        self.name_for_searching = name_for_searching
-        self.url_path = url_path
+    def __init__(self, name_for_searching: str):
+        self.name_for_searching = self.replace_name(name_for_searching)
+        self.url_path = Config.get_url_wiki()
 
-    def get_descriptions(self, name: str) -> str:
+
+    @staticmethod
+    def replace_name(name) -> str:
+        """
+        The function change a name to actually name in url
+        :name_for_change: Any name
+        :type: :obj: `str`
+        :return Name
+        :type: :obj: `str`
+        """
+        return name.replace(" ", "_")
+
+    def get_descriptions(self) -> str:
         """ Return a descriptions of the ship
             :return: `obj` str
             :type:
         """
-        my_response = requests.get(self.url_starship + name.replace(" ", "_"))
+        my_response = requests.get(self.url_path + self.name_for_searching)
         my_soup = BeautifulSoup(my_response.content, "lxml").select("div.mw-parser-output > p")
         result = ""
         for p in my_soup:
@@ -35,14 +47,17 @@ class Wiki:
                 break
         return result.strip()
 
-    def get_photo_ship(self, name: str) -> str:
+    def get_photo_wiki(self) -> str:
         """
         Return a photo of the starship
             :return: `obj` str
             :type: :obj: `str`
         """
-        my_response = requests.get(self.url_starship + name.replace(" ", "_"))
-        my_soup = BeautifulSoup(my_response.content, "lxml").select("div.mw-parser-output > p")
+        my_response = requests.get(self.url_path + self.name_for_searching)
+        if self.name_for_searching == "Droid":
+            my_soup = BeautifulSoup(my_response.content, "html.parser")
+        else:
+            my_soup = BeautifulSoup(my_response.content, "lxml").select("div.mw-parser-output > p")
         result = ""
         for p in my_soup:
             if "<aside" in str(p):
@@ -54,13 +69,40 @@ class Wiki:
         self.image_path = result
         return result
 
-    def download_image(self, path_photo: str) -> bool:
+    def get_specie_photo(self):
+        my_response = requests.get(self.url_path + self.name_for_searching)
+        # my_soup = BeautifulSoup(my_response.content, "lxml")
+        my_soup = BeautifulSoup(my_response.content, "html.parser")
+        result = []
+        for p in my_soup:
+            if "<aside" in str(p):
+                result.append(p.select_one("img.pi-image-thumbnail").get("src"))
+                break
+            else:
+                result.append(my_soup.select_one("img.thumbimage").get("data-src"))
+                break
+        self.image_path = result
+        return result
+    def download_image(self, path_photo: str = "") -> bool:
         """ The function save  image on your machine
             :param path_photo: Name and path to save
             :type path_photo: `obj` str
         """
-        self.get_photo_ship(self.get_name())
+        self.get_photo_wiki()
         if BaseRequest._check_status_code(self.image_path):
             with open(path_photo, 'wb') as file:
                 file.write(requests.get(self.image_path).content)
             return os.path.exists(path_photo)
+
+
+if __name__ == "__main__":
+    # payload = {"search_q": "Pau'an"}
+    # response = requests.get("https://www.starwars.com/search?q=Droid")
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # for element in soup.find_all('div', {'class': 'title'}):
+    #     print(element.a)
+    # wiki = Wiki("Y-wing")
+    wiki = Wiki("Droid")
+    # wiki = Wiki("Wookie")
+    # wiki = Wiki("Pau'an")
+    print(wiki.get_photo_wiki())
--- src/temp.py
+++ src/temp.py
@@ -1,32 +1,20 @@
-my_dict = {
-    "value": 1,
-    "next": {
-        "value": 2,
-        "next": {
-            "value": 3,
-            "next": None
-        }
-    }
-}
+# my_dict = {
+#     "value": 1,
+#     "next": {
+#         "value": 2,
+#         "next": {
+#             "value": 3,
+#             "next": None
+#         }
+#     }
+# }
 
-tmp = None
-while my_dict:
-        next = my_dict['next']
-        my_dict['next'] = tmp
-        tmp = my_dict
-        my_dict = next
-
-my_dict = tmp
-print(my_dict)
-
-# for key, value in my_dict.items():
-#     next = value.get("next")
+# tmp = None
+# while my_dict:
+#         next = my_dict['next']
+#         my_dict['next'] = tmp
+#         tmp = my_dict
+#         my_dict = next
 #
-#     print(key, value)
-
-# def rever(m_dict):
-#     m_dict = {}
-#
-#     m_dict["value"], m_dict.get("next")["value"] = m_dict.get("next").get("value"), m_dict.get("value")
-#     print(m_dict)
-# rever(my_dict)
+# my_dict = tmp
+# print(my_dict)
