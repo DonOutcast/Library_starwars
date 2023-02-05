0cfe540424ebe92208cd45d668549c743061f6c2
DonOutcast <lymondgl@student.21-school.ru>
2023-02-05 16:47:09 +0300

    test without raise

--- src/tests_star/test_vehicle.py
+++ src/tests_star/test_vehicle.py
@@ -4,8 +4,9 @@ from src.star_wars.utils.vehicle import Vehicles
 
 class StarWarsTest(unittest.TestCase):
     def test_specie(self):
-        vehicle = Vehicles(2)
+        vehicle = Vehicles(14)
         self.assertEqual(vehicle.get_films(), ['The Empire Strikes Back'], "should ['The Empire Strikes Back']")
+        # self.assertRaises()
         self.assertEqual(vehicle.get_pilots(), ['Luke Skywalker', 'Wedge Antilles'], "should ['Luke Skywalker', 'Wedge Antilles']")
 
 
