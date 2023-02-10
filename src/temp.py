9cfb749f0634647eef497a2990af40d7957940d7
DonOutcast <lymondgl@student.21-school.ru>
2023-02-11 00:12:31 +0300

    testin simpliest requests

--- src/temp.py
+++ src/temp.py
@@ -1,23 +1,18 @@
-my_dict = {
-    "value": 1,
-    "next": {
-        "value": 2,
-        "next": {
-            "value": 3,
-            "next": {
-                "value": 4,
-                "next": None
-            }
-        }
-    }
-}
+import requests
+from bs4 import BeautifulSoup
 
-tmp = None
-while my_dict:
-        m_next = my_dict['next']
-        my_dict['next'] = tmp
-        tmp = my_dict
-        my_dict = m_next
-
-my_dict = tmp
-print(my_dict)
+if __name__ == "__main__":
+    url = "https://www.starwars.com/films/star-wars-episode-iv-a-new-hope"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    # print(soup.find("section", {"id": "ref-1-6"}))
+    # print(soup.find_all("div", {"class": "module_header filters_header has-list has-dropdown"}))
+    # print(soup.select("#ref-1-6 > div.bound.vertical.peeking"))
+    r_1 = requests.get("https://www.starwars.com/_grill/filter/films/star-wars-episode-iv-a-new-hope?filter=Characters&mod=6")
+    r_2 = requests.get("https://www.starwars.com/_grill/filter/films/star-wars-episode-iv-a-new-hope?filter=Creatures&mod=6")
+    temp = r_2.json()
+    print(temp)
+    # for i in temp["data"]:
+    #     if i["slug"] == "databank/boba-fett":
+    #         print(i)
+        # print(i["id"], i["slug"],  i["desktop_square_thumb"])
